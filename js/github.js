503d65a1f75abcfecdb157d3e061f36f10adfd28
Noah <yasskin@icloud.com>
2016-08-05 09:36:30 -0700

    interface is communicating with the back-end GitHub object

--- js/github.js
+++ js/github.js
@@ -1,22 +1,10 @@
 var apiKey = require('./../.env').apiKey;
 
-
-GitHub = function() {
-};
-
-// GitHub.prototype.getName = function(username) {
-//   $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
-//     console.log(response[0].name);
-//   }).fail(function(error){
-//     console.log(error.responseJSON.message);
-//   });
-// }
+function GitHub(){
+}
 
 GitHub.prototype.getRepos = function(username) {
-  $.get('https://api.github.com/users/yasskin?access_token=1e37edbe2c0ad2017ed42a1b46db61d630aae26e').then(function(response) {
-  }).fail(function(error){
-    console.log(error.responseJSON.message);
-  });
+console.log("Communicating!");
 };
 
 exports.reposModule = GitHub;
--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -2,10 +2,6 @@ var apiKey = require('./../.env').apiKey;
 var GitHub = require('./../js/github.js').reposModule;
 
 $(document).ready(function() {
-  var gitUser = new GitHub();
-  $('#githubUsername').click(function() {
-    var username = $('username').val();
-    $('#username').val("");
-    gitUser.getRepos(username);
-  });
+  var gitUserObject = new GitHub();
+    gitUserObject.getRepos();
 });
